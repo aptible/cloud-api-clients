54397e44579186f552c6213c223088068002852c
Madhu <madhuravius@protonmail.com>
2022-10-07 13:44:55 -0400

    chore: replicated some partials for ref in use

---
+++ docs/examples/python/partials/elasticache/main.py
@@ -0,0 +1,52 @@
+import typing
+from typing import Optional
+
+# below is done to enable testing outside the scope of the project with ease
+import sys
+sys.path.insert(1, '../../../clients/python')
+from aptible_client.helpers import getters, logger_utils, waiters  # NOQA
+
+if typing.TYPE_CHECKING:
+    from clients.python.aptible_client.helpers import getters, logger_utils, waiters
+
+
+logger = logger_utils.setup_logger()
+
+VPC_NAME: str = "testing-aptible-client"
+
+
+def main(
+    environment_id: str,
+    organization_id: str,
+    vpc_name: Optional[str] = VPC_NAME,
+    do_unique_checks: Optional[bool] = True
+):
+    logger.info("Starting the elasticache flow")
+    configuration = getters.get_client_configuration()
+    # Initialize the API and create a session
+    waiter = waiters.Waiter(
+        configuration=configuration,
+        environment_id=environment_id,
+        logger=logger,
+        organization_id=organization_id,
+        do_unique_checks=do_unique_checks
+    )
+
+    # Launch VPC
+    waiter.get_or_launch_asset_and_wait(
+        asset="aws__vpc__latest",
+        asset_parameters={
+            "name": vpc_name
+        }
+    )
+
+    # Launch ElastiCache Redis cache
+    waiter.get_or_launch_asset_and_wait(
+        asset="aws__elasticache_redis__latest",
+        asset_parameters={
+            "name": "app-redis",
+            "snapshot_window": "00:00-01:00",
+            "maintenance_window": "sun:10:00-sun:14:00",
+            "vpc_name": vpc_name
+        },
+    )
---
+++ docs/examples/python/partials/vpc/main.py
@@ -0,0 +1,43 @@
+import typing
+from typing import Optional
+
+
+# below is done to enable testing outside the scope of the project with ease
+import sys
+sys.path.insert(1, '../../../clients/python')
+from aptible_client.helpers import getters, logger_utils, waiters  # NOQA
+
+if typing.TYPE_CHECKING:
+    from clients.python.aptible_client.helpers import getters, logger_utils, waiters
+
+
+logger = logger_utils.setup_logger()
+
+VPC_NAME: str = "testing-aptible-client"
+
+
+def main(
+    environment_id: str,
+    organization_id: str,
+    vpc_name: Optional[str] = VPC_NAME,
+    do_unique_checks: Optional[bool] = True
+):
+    logger.info("Starting the create vpc flow")
+    configuration = getters.get_client_configuration()
+    # Initialize the API and create a session
+    waiter = waiters.Waiter(
+        configuration=configuration,
+        environment_id=environment_id,
+        logger=logger,
+        organization_id=organization_id,
+        do_unique_checks=do_unique_checks
+    )
+
+    # Launch VPC
+    waiter.get_or_launch_asset_and_wait(
+        asset="aws__vpc__latest",
+        asset_parameters={
+            "name": vpc_name
+        }
+    )
+
